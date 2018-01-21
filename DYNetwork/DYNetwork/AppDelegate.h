f1b5de3360b5870724135acc27045358e222a002
郑良凯 <liangkai.zheng@datayes.com>
2018-01-21 16:32:34 +0800

    init

---
+++ DYNetwork/DYNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYNetwork
+//
+//  Created by 郑良凯 on 2018/1/21.
+//  Copyright © 2018年 flame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYNetwork/DYNetwork/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DYNetwork
+//
+//  Created by 郑良凯 on 2018/1/21.
+//  Copyright © 2018年 flame. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
